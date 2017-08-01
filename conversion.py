b48448ff24f7537736daf836af07fb6f288d974e
prestonh <tphinkle@gmail.com>
2017-08-01 12:06:27 -0700

    added a conversion.py file

---
+++ conversion.py
@@ -0,0 +1,11 @@
+# Convert units
+
+def um_to_nm(um):
+    '''
+    Converts microns to nanometers.
+    Arguments: Number in microns to be converted.
+    Return: Number in nanometers after conversion.
+    '''
+
+    nm = um*1000.
+    return nm
